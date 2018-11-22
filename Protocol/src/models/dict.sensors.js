80bd9d61cd4df603c9604ca470433eb1defe4e16
Roman Mashenkin <xromash@vxdesign.store>
2018-11-23 02:28:22 +0300

    Apply reviewer's comment

--- Protocol/src/models/dict.sensors.js
+++ Protocol/src/models/dict.sensors.js
@@ -1,5 +1,7 @@
 const sensors = {
     LED_ONE_COLOR: "led.one_color",
     SENSOR_TEMPERATURE: "sensor.temperature",
-    SENSOR_HUMIDITY: "sensor.humidity"
+    SENSOR_HUMIDITY: "sensor.humidity",
+    SENSOR_RECEIVER: "sensor.receiver",
+    SENDOR_SENDER: "sensor.sender"
 }
