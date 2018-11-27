4be7f2ab51e2157f9c63ca1fcffa100e145b7500
Anna Boykova <boykova_anuta@mail.ru>
2018-11-28 02:32:14 +0300

    [#10, #11] Change json file folder

--- Server/src/backend/profile/config.js
+++ Server/src/backend/profile/config.js
@@ -1,6 +1,6 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('../profile.json')
+const adapter = new FileSync('profile.json')
 const db = low(adapter)
 const Profile = require("./profile.js")
 
