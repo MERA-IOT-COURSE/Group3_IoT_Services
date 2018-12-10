1d9b30c322173e5bb1f569e6a3e4916aee367b45
Roman Mashenkin <xromash@vxdesign.store>
2018-12-10 16:16:18 +0300

    [#67] Update models that require array instead of null

--- Common/src/models/protocol/entities/device.js
+++ Common/src/models/protocol/entities/device.js
@@ -9,11 +9,11 @@ class Device extends Entity {
         var name = data["name"]
         var sensors = data["sensors"].map((sensor) => { return Sensor.parse(sensor) })
         var actions = data.hasOwnProperty("actions") && data["actions"] !== null ?
-            data["actions"].map((action) => { return Action.parse(action) }) : null
+            data["actions"].map((action) => { return Action.parse(action) }) : []
         return new Device(id, name, sensors, actions)
     }
 
-    constructor(id, name, sensors, actions = null) {
+    constructor(id, name, sensors, actions = []) {
         super()
         this.id = id
         this.name = name
@@ -27,7 +27,7 @@ class Device extends Entity {
             "name": this.name,
             "sensors": this.sensors.map((sensor) => { return sensor.properties() })
         }
-        if (this.actions != null) {
+        if (this.actions != null && this.actions.length > 0) {
             result["actions"] = this.actions.map((action) => { return action.properties() })
         }
         return result
