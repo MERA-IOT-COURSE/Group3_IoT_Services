976cd307e5aed93f7adb17ff0785d662a99a7914
Roman Mashenkin <xromash@vxdesign.store>
2018-12-16 18:51:36 +0300

    [#10] Implement ConfigurationProfile for Server module

--- Server/src/backend/profile/profile.js
+++ Server/src/backend/profile/profile.js
@@ -1,9 +1,10 @@
-const Profile = require("common").models.containers.Profile
-
-class ServerProfile extends Profile {
-    constructor(options = {}) {
-        super(options)
-    }
+module.exports = (database) => {
+    return new database.Schema({
+        backendId: { type: String, default: "group3-be" },
+        broker: {
+            host: { type: String, default: "localhost" },
+            port: { type: Number, default: 1883 }
+        },
+        active: { type: Boolean, default: true }
+    })
 }
-
-module.exports = ServerProfile
