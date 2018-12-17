bd33e48b0970a5302c796679bfbf7406396783b4
Roman Mashenkin <xromash@vxdesign.store>
2018-12-17 20:06:59 +0300

    Create Server view and connect with BE side

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,22 @@
 const common = require("common")
 const launcher = common.utils.web.launcher
 const Server = common.models.containers.Server
+const MongoDB = common.models.containers.MongoDB
 
+const ConfigurationProfile = require("./src/backend/profile/config")
 const Requests = require("./src/backend/web/requests")
 
 const static = require("path").join(__dirname, "./src")
-const requests = new Requests()
-const server = new Server({
-    static: static,
-    requests: requests
-})
 
-launcher(server)
+const mongoDb = new MongoDB({ user: "user", password: "user" })
+const configurationProfile = new ConfigurationProfile(mongoDb)
+configurationProfile.initialize().then(() => {
+    const requests = new Requests(configurationProfile)
+
+    const server = new Server({
+        static: static,
+        requests: requests
+    })
+
+    launcher(server)
+})
--- Server/src/backend/web/requests.js
+++ Server/src/backend/web/requests.js
@@ -1,9 +1,41 @@
 const AbstractRequests = require("common").models.containers.AbstractRequests
 
 class Requests extends AbstractRequests {
+    constructor(configurationProfile) {
+        super()
+        this.configurationProfile = configurationProfile
+    }
+
     get(app) {
-        app.get("/", (req, res) => {
-            res.render("main")
+        app.get("/", async (req, res) => {
+            var profile = await this.configurationProfile.get()
+            res.render("main", {
+                profile: profile
+            })
+        })
+    }
+
+    put(app) {
+        app.put("/profile", async (req, res) => {
+            var profile = await this.configurationProfile.get()
+            for (var key in req.body) {
+                switch (key) {
+                    case "backendId":
+                        profile.backendId = req.body[key]
+                        break
+                    case "broker[host]":
+                        profile.broker.host = req.body[key]
+                        break
+                    case "broker[port]":
+                        profile.broker.port = req.body[key]
+                        break
+                    case "active":
+                        profile.active = req.body[key] === "true"
+                        break
+                }
+            }
+            await this.configurationProfile.update(profile)
+            res.send()
         })
     }
 }
