2a7ee9e9aee555f4ede22c3a8197a907c9b7266f
Roman Mashenkin <xromash@vxdesign.store>
2018-12-17 18:12:31 +0300

    Apply linter remarks

--- Device/src/frontend/sensor.add.js
+++ Device/src/frontend/sensor.add.js
@@ -1,15 +1,15 @@
 $(function () {
     $("button#saveSensorAddForm").click(function (event) {
-        var sensorId = $(`#sensorID`).val()
-        var sensorType = $(`#sensorType`).val()
+        var sensorId = $("#sensorID").val()
+        var sensorType = $("#sensorType").val()
         var actions = []
-        var actionsSensorCommon = $(`#actionsSensorCommon`).val()
+        var actionsSensorCommon = $("#actionsSensorCommon").val()
         if (actionsSensorCommon.length > 1) {
             actions = actions.concat(actionsSensorCommon)
         } else if (actionsSensorCommon.length == 1) {
             actions.push(actionsSensorCommon[0])
         }
-        var actionsSensorCustom = $(`#actionsSensorCustom`).val()
+        var actionsSensorCustom = $("#actionsSensorCustom").val()
         if (actionsSensorCustom.length > 1) {
             actions = actions.concat(actionsSensorCustom)
         } else if (actionsSensorCustom.length == 1) {
