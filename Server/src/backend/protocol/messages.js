cd88fde7632d5601c2eaa4f10c1a46c4b1b5737e
Roman Mashenkin <xromash@vxdesign.store>
2018-12-22 01:58:52 +0300

    Use save method for Server app with async/await keywords

--- Server/src/backend/protocol/messages.js
+++ Server/src/backend/protocol/messages.js
@@ -40,6 +40,14 @@ class ServerMessages extends Messages {
         }
     }
 
+    async saveIncoming(topic, message, data, datetime = new Date()) {
+        await save(topic, message, data, "INCOMING", datetime = new Date())
+    }
+
+    async saveOutcoming(topic, message, data, datetime = new Date()) {
+        await save(topic, message, data, "OUTCOMING", datetime = new Date())
+    }
+
     async save(topic, message, data, comment, datetime = new Date()) {
         data["topic"] = topic
         data["comment"] = comment
