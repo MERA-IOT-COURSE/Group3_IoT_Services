e26ba5bd34ece944d4e6d7cdc3276072b5f0b139
Roman Mashenkin <xromash@vxdesign.store>
2018-12-23 01:45:43 +0300

    Fix issues that related to messages modal

--- Device/src/frontend/messages.load.js
+++ Device/src/frontend/messages.load.js
@@ -1,13 +1,15 @@
-$("#messagesModalCenter").on("show.bs.modal", function (event) {
-    $.ajax({
-        type: "GET",
-        url: "/messages",
-        success: function (res) {
-            $(".modal-body").html(res)
-        },
-        error: function () {
-            alert("Failed")
-            window.location.reload()
-        }
+$(function () {
+    $("#messagesModalCenter").on("show.bs.modal", function (event) {
+        $.ajax({
+            type: "GET",
+            url: "/messages",
+            success: function (res) {
+                $(".modal-body").html(res)
+            },
+            error: function () {
+                alert("Failed")
+                window.location.reload()
+            }
+        })
     })
 })
